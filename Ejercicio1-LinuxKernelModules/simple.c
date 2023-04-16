092ecd82e18cee80eb01c22505325b9ca6965d35
NickGalindo <ngalindoc@unal.edu.co>
2023-04-15 23:43:59 -0500

    stuf

--- Ejercicio1-LinuxKernelModules/simple.c
+++ Ejercicio1-LinuxKernelModules/simple.c
@@ -1,15 +1,23 @@
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/hash.h>
+#include <linux/gcd.h>
+#include <asm/param.h>
+#include <linux/jiffies.h>
 
 int simple_init(void){
   printk(KERN_INFO "Loading Das Kernel Modulus\n");
+  printk(KERN_INFO "GOLDEN_RATIO_PRIME: %llu\n", GOLDEN_RATIO_PRIME);
+  printk(KERN_INFO "Jiffies: %lu Hz: %d\n", jiffies, HZ);
 
   return 0;
 }
 
 void simple_exit(void){
   printk(KERN_INFO "Removing Kernel Module\n");
+  printk(KERN_INFO "GCD(3300, 24): %lu\n", gcd(3300, 24));
+  printk(KERN_INFO "Jiffies: %lu", jiffies);
 }
 
 module_init(simple_init);
